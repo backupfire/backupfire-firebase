36873311c390fa58f7b4bd77657070835059ae26
Sasha Koss <koss@nocorp.me>
2019-09-25 14:01:41 +0300

    Point the test server to the new staging URL

--- test/server/index.ts
+++ test/server/index.ts
@@ -4,8 +4,7 @@ import * as admin from 'firebase-admin'
 admin.initializeApp()
 
 export const backup = backupfire({
-  controllerToken: 'qwe',
-  controllerDomain: 'backup-fire-staging.firebaseapp.com',
-  adminPassword: 'asd',
-  bucketsWhitelist: []
+  controllerToken: 'jc3PJUnjp45FIZyCPwCGWtM2LZ0rLQlORtDDpoIIN620pzvI',
+  controllerDomain: 'staging.backupfire.dev',
+  adminPassword: 'misstep-overt-woodyard-fest-zinc-persist'
 })
