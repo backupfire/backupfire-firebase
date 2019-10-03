a1313b4440be470f0800aa19ec6f6d0965d07aa5
Sasha Koss <koss@nocorp.me>
2019-10-03 19:31:31 +0200

    Move credentials to the environment config

--- test/server/index.ts
+++ test/server/index.ts
@@ -3,8 +3,4 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp()
 
-export const backup = backupfire({
-  controllerToken: 'jc3PJUnjp45FIZyCPwCGWtM2LZ0rLQlORtDDpoIIN620pzvI',
-  controllerDomain: 'staging.backupfire.dev',
-  adminPassword: 'misstep-overt-woodyard-fest-zinc-persist'
-})
+export const backup = backupfire()
