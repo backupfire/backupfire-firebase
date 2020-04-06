223c09f97212f2b4f64d01cc7f34f93ad67f1a8c
Sasha Koss <koss@nocorp.me>
2020-04-06 15:04:47 +0300

    Update examples
    Add admin.initializeApp() call to examples

--- examples/js/functions/index.js
+++ examples/js/functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 // 1. Import the agent package
 const backupfireAgent = require('@backupfire/firebase')
+
+admin.initializeApp()
 
 exports.helloWorld = functions.https.onRequest((_request, response) => {
   response.send('Hello from Firebase!')
--- examples/ts/functions/src/index.ts
+++ examples/ts/functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 // 1. Import the agent package
 import backupfireAgent from '@backupfire/firebase'
+
+admin.initializeApp()
 
 exports.helloWorld = functions.https.onRequest((_request, response) => {
   response.send('Hello from Firebase!')
